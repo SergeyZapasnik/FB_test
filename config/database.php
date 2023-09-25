083e9c1747851639d155fbe756335b1460c1afba
Sergey Zapasnik <zapseta@gmail.com>
2023-09-25 18:27:47 +0200

    Create config/database.php for database connection settings

---
+++ config/database.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'driver' => 'mysql',        // Database driver (e.g., mysql, postgresql, sqlite)
+    'host' => 'localhost',      // Database server address
+    'database' => 'your_db',
+    'username' => 'your_user',
+    'password' => 'your_pass',
+    'charset' => 'utf8mb4',     // Character encoding (UTF-8 recommended)
+    'collation' => 'utf8mb4_unicode_ci', // Collation (UTF-8 Unicode recommended)
+];
